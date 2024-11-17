6a5f84e28d5521fcde3e62ecdcc75d72845aefac
PHzeek <156956984+PHzeek@users.noreply.github.com>
2024-11-17 04:00:43 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ st.write('Below is a Dataframe:', df, 'Above is a dataframe.')
 
 df2 = pd.DataFrame(
     np.random.randn(200,3),
-    column=['a', 'b', 'c'])
+    columns=['a', 'b', 'c'])
 
 c = alt.Chart(df2).mark_circle().encode(
     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
